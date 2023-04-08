052bfbea81ccaa1b7ae52db798271f02caf99e02
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-04-08 10:32:16 +0100

    head element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import HeadElement from '@/components/layout/HeadElement';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeadElement />
+      <Component {...pageProps} />
+    </>
+  );
 }
