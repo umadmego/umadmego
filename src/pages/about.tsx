c6c36e12dd848899c9ffdc9a762ec6ddd6a68e16
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-09-18 20:44:04 +0100

    about header

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,13 @@
+import AboutHeader from '@/components/aboutPage/AboutHeader';
 import AppLayout from '@/components/layout/AppLayout';
 import React from 'react';
 
 function About() {
-  return <AppLayout>About</AppLayout>;
+  return (
+    <AppLayout>
+      <AboutHeader />
+    </AppLayout>
+  );
 }
 
 export default About;
