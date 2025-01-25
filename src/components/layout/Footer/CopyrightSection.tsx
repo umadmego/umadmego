ec85c3e6527ee88f865b1c7a2bc974319f8ed560
Alves <victorsouza072004@gmail.com>
2025-01-25 00:02:45 -0300

    ajustando

--- src/components/layout/Footer/CopyrightSection.tsx
+++ src/components/layout/Footer/CopyrightSection.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function CopyrightSection() {
   return (
-    <div className='bg-[#272727] p-5 text-center text-white text-sm font-normal mt-[71px]'>
-      &copy; 2023 The Father's House Church
+    <div className="bg-[#272727] p-5 text-center text-white text-sm font-normal mt-[71px]">
+      &copy; 2k25 Umadmego
     </div>
   );
 }
