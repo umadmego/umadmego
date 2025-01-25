953b3b0b8ed6906d32d425eb8bc836d38a6a9ce8
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2025-01-25 03:40:33 -0300

    Versão WEB

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     SESSION_NAME: 'TFH Web',
     SESSION_KEY: 'TFH_WEB_USER',
     API_URL: 'https://www.goldenogbeka.com/api/v2',
-    // API_URL: 'http://localhost:5000/api/v2',
     API_KEY: 'TFH_ADMIN',
     YOUTUBE_API_KEY: 'AIzaSyBR2Wg2mC2BTHj0ONibXzPyNpnhL2Egjxg',
     YOUTUBE_UPLOAD_KEY: 'UU4jT_fVaY8Yf_FkWaDphPPg',
@@ -13,7 +12,9 @@ const nextConfig = {
   },
   images: {
     domains: ['i.ytimg.com', 'res.cloudinary.com'],
+    unoptimized: true,
   },
+  output: 'export',
 };
 
 module.exports = nextConfig;
